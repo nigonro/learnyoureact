6f12ad2d6634382654e76fd9b3376a78a8f7dbbd
Nicolas Gonzalez Rodriguez <nigonro@rrz.cc>
2016-08-01 00:12:58 +0200

    CSS

--- views/index.jsx
+++ views/index.jsx
@@ -44,11 +44,11 @@ class Todo extends React.Component {
   render() {
     return (
       <tr>
-        <td style={{border: "1px solid black"}}>
+        <td style={style.tableContent}>
         <input type="checkbox" checked={this.state.checked} onChange={this.handleChange.bind(this)}/>
         </td>
-        <td style={{border: "1px solid black"}}>{this.props.title}</td>
-        <td style={{border: "1px solid black"}}>{this.props.children}</td>
+        <td style={style.tableContent}>{this.props.title}</td>
+        <td style={style.tableContent}>{this.props.children}</td>
       </tr>
     );
   }
@@ -67,4 +67,10 @@ class TodoForm extends React.Component {
     );
   }
 }
+
+let style = {
+  tableContent: {
+    border: "1px solid black"
+  }
+};
 
