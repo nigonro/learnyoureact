184c0303fa3ce49041e420dbe558af7db8b53fb9
Nicolas Gonzalez Rodriguez <nigonro@rrz.cc>
2016-07-31 17:41:46 +0200

    Hello React & Components

---
+++ program.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+app.set('port', (process.argv[2] || 3000));
+app.set('view engine', 'jsx');
+app.set('views', __dirname + '/views');
+app.engine('jsx', require('express-react-views').createEngine({ transformViews: false  }));
+
+require('babel/register')({
+  ignore: false
+});
+
+app.use('/', function(req, res) {
+  res.render('index', '');
+});
+
+app.listen(app.get('port'), function() {});
